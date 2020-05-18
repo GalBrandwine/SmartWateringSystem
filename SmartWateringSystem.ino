05376adcba773e09c8ab0dcf86afb6871fe6ea16
GalBrandwine <Fs5(r3NL=/vS-s>
2020-05-18 21:04:10 +0300

    Added offline developmentd - that fixed all bugs.

--- SmartWateringSystem.ino
+++ SmartWateringSystem.ino
@@ -5,7 +5,7 @@
  * 1 byte for 'runtime', the other for 'frequency'.
  * That means that maximum 'runtime'/'frequency' value will be 255, moreover - I can operate the valve for hole minutes, e.g 0.5 minute wont work - I cant store that in a BYTE
 */
-#define EEPROM_SIZE 2
+#define EEPROM_SIZE 3
 
 
 #include <ArduinoOTA.h>
@@ -25,7 +25,6 @@
 
 #define VALVE_SWITCH_PIN 18
 #define MOTOR_PIN 19
-//#define LED_BUILTIN 2   // Set the GPIO pin where you connected your test LED or comment this line out if your dev board has a built-in LED
 
 
 // Set these to your desired credentials.
@@ -34,155 +33,159 @@ const char *password  = "AtzizHaham";
 
 AsyncWebServer server(80);
 
-//String ledState       = "ON";
-//String valve_position = "Closed";
 
+SemaphoreHandle_t xMutex;
 /* ****************************************************************************
  * ********************************** Some globals ****************************
  * ****************************************************************************
 */
+enum VALVE_MODES{
+  timer,
+  valve_open,
+  valve_close
+};
+VALVE_MODES valveMode = timer;
+
 bool isSystemWatering = false;
 
-#define RUNTIME_OFF_VALUE -1
+#define RUNTIME_OFF_VALUE 255
 #define RUNTIME_ON_VALUE 254
 int runtime = RUNTIME_OFF_VALUE;
 
-#define FREQUENCY_RESET_VALUE -1
+#define FREQUENCY_RESET_VALUE 0
 int frequency_hours = FREQUENCY_RESET_VALUE;
 int time_left_until_next_watering_min = frequency_hours*60;
 
-  
-void runtimeTask(void * parameter){
-  int runtime_temp = runtime;
-  
-  Serial.print("runtimeTask::runtime ");Serial.println(runtime_temp);
-  if (runtime_temp == RUNTIME_OFF_VALUE){
-    Serial.print("runtimeTask::runtime is RUNTIME_OFF_VALUE");Serial.println(RUNTIME_OFF_VALUE);
-    vTaskDelete(NULL);
-    return;
-  }
-  else{// If runtime_temp is RUNTIME_ON_VALUE (254) so valve will be open for 254 minutes and not forever.
-    isSystemWatering = true;
-    openForXMinute(runtime_temp);
-  }
-  Serial.println("runtimeTask::after openForXMinute");
 
-  isSystemWatering = false;
-  
-  // When you're done, call vTaskDelete. Don't forget this!
-  vTaskDelete(NULL);
-}
+//void runtimeTask(void * parameter){
+//  int runtime_temp = *((int*)parameter);
+//
+//  isSystemWatering = true;
+//  openForXMinute(runtime_temp);
+//  isSystemWatering = false;
+//
+//  // When you're done, call vTaskDelete. Don't forget this!
+//  vTaskDelete(NULL);
+//}
 
 
 void frequencyTask( void* pvParameters )
-{ 
+{
   BaseType_t xReturned;
-  // This TaskHandle will allow 
+  // This TaskHandle will allow
   TaskHandle_t xHandle = NULL;
   int seconds = 0;
+  int valveTime = 0;
   int hours_passed_from_last_watering = 0; //max watering interval is 168 hours (a week)
-  
+
   for (;;)
   {
     vTaskDelay( pdMS_TO_TICKS( 1000 ) );
     seconds++;
-    
-//    if (seconds >= 3600){
-    if (seconds % 3600 == 0){
+
+    if (seconds % 3600 == 0){//10
       hours_passed_from_last_watering++;
-//      seconds = 0;
-    }
-    
-    if (hours_passed_from_last_watering >= frequency_hours){ // Time to water the plants.
-      if (!isSystemWatering){
-          Serial.println("frequencyTask::watering ");
-          
-          xReturned = xTaskCreate(
-              runtimeTask,        // Function that should be called
-              "Watering task",    // Name of the task (for debugging)
-              1000,               // Stack size (bytes)
-              NULL,               // Parameter to pass
-              1,                  // Task priority
-              &xHandle   // Task handle
-          );
-        }
-      
-      
-//      seconds = 0;
-      hours_passed_from_last_watering = 0;
     }
 
-    if (runtime == RUNTIME_ON_VALUE){ // Force valve opening.
-      if (!isSystemWatering){
-          Serial.println("frequencyTask::Force valve opening ");
-          
-          xReturned = xTaskCreate(
-              runtimeTask,        // Function that should be called
-              "Watering task",    // Name of the task (for debugging)
-              1000,               // Stack size (bytes)
-              NULL,               // Parameter to pass
-              1,                  // Task priority
-              &xHandle   // Task handle
-          );
-        }
-    }
-    if (runtime == RUNTIME_OFF_VALUE && isSystemWatering){ // Force valve closing.
-        Serial.println("frequencyTask::Force valve closing ");
-        // Kill task1 if it's running
-        if( xReturned == pdPASS) {  // If the task was created successfully then pdPASS is returned.
-          vTaskDelete(xHandle);
+//    Serial.print("frequencyTask::isSystemWatering: ");Serial.println(isSystemWatering);
+//    Serial.print("frequencyTask::FREQUENCY_RESET_VALUE: ");Serial.println(FREQUENCY_RESET_VALUE);
+//    Serial.print("frequencyTask::frequency_hours: ");Serial.println(frequency_hours);
+//    Serial.print("frequencyTask::frequency_hours in seconds: ");Serial.println(frequency_hours*3600);
+//    Serial.print("frequencyTask::seconds passed from last watering: ");Serial.println(seconds);
+//    Serial.print("frequencyTask::hours_passed_from_last_watering: ");Serial.println(hours_passed_from_last_watering);
+//    Serial.print("frequencyTask::valveTime: ");Serial.println(valveTime);
+//    Serial.println("");
 
-          closeValve();
-          isSystemWatering = false;
-          
-        }
+
+
+    if (frequency_hours != FREQUENCY_RESET_VALUE){
+        seconds                           = seconds % (frequency_hours*3600);       // Frequency_hours in seconds. (If frequency_hours==0) then it throws DivisionByZero!!)
+        time_left_until_next_watering_min = frequency_hours*60 - seconds/60;        // hours_passed_from_last_watering;
+    }
+    else {
+        seconds                         = 0;
+        valveTime                       = seconds;
+        hours_passed_from_last_watering = 0;
+
+        Serial.println("frequencyTask::hours_passed_from_last_watering RESETED ");
     }
 
-    seconds = seconds % (frequency_hours*60);
-    time_left_until_next_watering_min = frequency_hours*60 - seconds/60;//hours_passed_from_last_watering;
-    
-    if (frequency_hours == FREQUENCY_RESET_VALUE){
-      hours_passed_from_last_watering = 0;
-      Serial.print("frequencyTask::hours_passed_from_last_watering RESETED ");
-      
+    xSemaphoreTake( xMutex, portMAX_DELAY ); //start critical section
+    switch (valveMode) {
+      case VALVE_MODES::timer:
+        Serial.print(seconds - valveTime);Serial.print(" ");Serial.println(runtime*60);
+        if (seconds - valveTime>= runtime*60){
+          if (closeValve()){
+            isSystemWatering    = false;
+            valveTime = 0;
+          }
+        }
+        if (hours_passed_from_last_watering >= frequency_hours && frequency_hours != FREQUENCY_RESET_VALUE){ // Time to water the plants.
+          Serial.println("frequencyTask::TIME TO WATER THE PLANTS");
+          if (!isSystemWatering){
+            if (openValve()){
+              isSystemWatering    = true;
+              hours_passed_from_last_watering = 0;
+              valveTime = seconds;
+            }
+          }
+        }
+        break;
+      case VALVE_MODES::valve_open:
+        if (openValve()){
+          isSystemWatering    = true;
+          valveTime = seconds;
+        }
+        break;
+      case VALVE_MODES::valve_close:
+        if (closeValve()){
+          isSystemWatering    = false;
+        }
+        break;
+      default:
+        // statements
+        break;
     }
+    xSemaphoreGive( xMutex ); // end start critical section
   }
-  
+
   vTaskDelete( NULL );
 }
 
 
-void closeValve() {
-    Serial.println("closing");  
+bool closeValve() {
+  Serial.println("closing");
   Serial.println(digitalRead(VALVE_SWITCH_PIN));
   while (digitalRead(VALVE_SWITCH_PIN) == 1) {
-//    Serial.println("closing");
     digitalWrite(MOTOR_PIN, LOW); // OPEN the MOSFET
-    delay(1);
+    delay(100);
   }
-  digitalWrite(MOTOR_PIN, HIGH);  // CLOSE the MOSFET 
+
+  digitalWrite(MOTOR_PIN, HIGH);  // CLOSE the MOSFET
   Serial.println("Valve closed");
+  return true;
 }
 
 
-void openValve() {
+bool openValve() {
   Serial.println("opening");
   Serial.println(digitalRead(VALVE_SWITCH_PIN));
   while (digitalRead(VALVE_SWITCH_PIN) == 0) {
-//    Serial.println("opening");
-    digitalWrite(MOTOR_PIN, LOW);               
-    delay(1);
+    digitalWrite(MOTOR_PIN, LOW);
+    delay(100);
   }
+
   Serial.println("Valve opened");
   digitalWrite(MOTOR_PIN, HIGH);
+  return true;
 }
 
 
 void openForXMinute(int minutes) {
-  
+
   const TickType_t xDelay = minutes*60000 / portTICK_PERIOD_MS; // X minutes
   Serial.print("openForXMinute::Opening valve for "); Serial.print(minutes); Serial.println(" minutes.");
-  
+
   openValve();
   vTaskDelay( xDelay);
 //  for ( uint32_t tStart = millis();  (millis() - tStart) < period;  ) {} // Simple buisy-wait loop. using milis() instead of delay() wont halt the arduino. watch dog dont like this one.
@@ -191,33 +194,45 @@ void openForXMinute(int minutes) {
 
 
 void processRuntimeRequest(const AsyncWebParameter* p){
+    xSemaphoreTake( xMutex, portMAX_DELAY );
     Serial.println();
     Serial.println("processRuntimeRequest::GOT runtine");
     Serial.println(p->name());
     Serial.println(p->value());
-    
+
     // Check If is is diffirent from previus runtime stored in memory:
     if (p->value().equals("off")) {
+      valveMode = VALVE_MODES::valve_close;
+      EEPROM.write(2, valveMode);
+
       runtime = RUNTIME_OFF_VALUE;
       EEPROM.write(0, RUNTIME_OFF_VALUE);
       EEPROM.commit();
     }
     else if (p->value().equals("on")) {
+      valveMode = VALVE_MODES::valve_open;
+      EEPROM.write(2, valveMode);
+
       runtime = RUNTIME_ON_VALUE;
       EEPROM.write(0, RUNTIME_ON_VALUE);
-      EEPROM.commit();      
+      EEPROM.commit();
     }
     else if (p->value().toInt() != runtime){
+      valveMode = VALVE_MODES::timer;
+      EEPROM.write(2, valveMode);
+
       runtime = p->value().toInt();
       EEPROM.write(0, p->value().toInt());
       EEPROM.commit();
     }
-    
+
     Serial.println("Runtime saved in flash memory");
+    xSemaphoreGive( xMutex );
 }
 
 
 void processFrequencyRequest(const AsyncWebParameter* p){
+    xSemaphoreTake( xMutex, portMAX_DELAY );
     Serial.println();
     Serial.println("processFrequencyRequest::GOT frequency");
     Serial.println(p->name());
@@ -225,10 +240,10 @@ void processFrequencyRequest(const AsyncWebParameter* p){
 
     if (p->value().equals("reset")) {
       EEPROM.write(1, FREQUENCY_RESET_VALUE);
-      EEPROM.commit();      
+      EEPROM.commit();
       Serial.println("processFrequencyRequest::Frequency RESETED");
     }
-    
+
     // Check If is is different from previous runtime stored in memory:
     if (p->value().toInt() != frequency_hours){
       frequency_hours = p->value().toInt();
@@ -236,6 +251,7 @@ void processFrequencyRequest(const AsyncWebParameter* p){
       EEPROM.commit();
       Serial.println("processFrequencyRequest::Frequency saved in flash memory");
     }
+    xSemaphoreGive( xMutex );
 }
 
 
@@ -243,39 +259,21 @@ void init(){
   initPints();
   initEEPROM();
 
-  // A one-off task. https://www.savjee.be/2020/01/multitasking-esp32-arduino-freertos/
-  xTaskCreate(
-      initValvePosition,              // Function that should be called
-      "Initiating valves position",   // Name of the task (for debugging)
-      1000,                           // Stack size (bytes)
-      NULL,            // Parameter to pass
-      1,               // Task priority
-      NULL             // Task handle
-  );
-
-  // An infinite task.
-  xTaskCreate(
-      frequencyTask,                    // Function that should be called
-      "Initiating frequencyTask",       // Name of the task (for debugging)
-      1000,                             // Stack size (bytes)
-      NULL,            // Parameter to pass
-      1,               // Task priority
-      NULL             // Task handle
-  );
-  
+  initValvePosition();
   initAccessPoint();
 }
 
 
 void initPints(){
   Serial.println("initPints::Initiating pins");
-//  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(MOTOR_PIN, OUTPUT);
   pinMode(VALVE_SWITCH_PIN, INPUT);
 }
 
 
 void initEEPROM(){
+//  writeToEeeprom();
+
   // initialize EEPROM with predefined size
   Serial.println();
   Serial.println("initEEPROM::Configuring EEPROM...");
@@ -284,22 +282,41 @@ void initEEPROM(){
   // Restore runtime and frequency from memory.
   runtime = EEPROM.read(0);
   frequency_hours = EEPROM.read(1);
-
+  valveMode = (VALVE_MODES)EEPROM.read(2);
   Serial.println("initEEPROM::Restoring from memory:");
   Serial.print("initEEPROM::Runtime:");Serial.println(runtime);
   Serial.print("initEEPROM::Frequency:");Serial.println(frequency_hours);
+  Serial.print("initEEPROM::valveMode:");Serial.println(valveMode);
 }
 
 
-void initValvePosition(void * parameter){
+void writeToEeeprom(){
+  // Debug mode.
+
+  Serial.println("writeToEeeprom::Debug mode");
+  EEPROM.write(0, RUNTIME_OFF_VALUE);
+  EEPROM.commit();
+
+
+  EEPROM.write(1, FREQUENCY_RESET_VALUE);
+  EEPROM.commit();
+
+  EEPROM.write(2, VALVE_MODES::timer);
+  EEPROM.commit();
+
+}
+
+bool initValvePosition(){
   // Resetting valve position
   Serial.println();
   Serial.println("initValvePosition::Reseting valve position...");
-  openValve();
-  closeValve();
+
+//  openValve();
+//  closeValve();
 
   // When you're done, call vTaskDelete. Don't forget this!
-  vTaskDelete(NULL);
+  // vTaskDelete(NULL);
+  return openValve() && closeValve();
 }
 
 
@@ -332,15 +349,15 @@ bool setUpServer(){
     // Take 'runtime' parameter from the request object.
     AsyncWebParameter* p = request->getParam(0);
     processRuntimeRequest(p);
-    
+
     // Send back to client "OK" message, and the desired value (as a confirmation)
     if(runtime == RUNTIME_OFF_VALUE){
-      request->send_P(200, "text/plain", "off"); 
+      request->send_P(200, "text/plain", "off");
     }
     else{
-      request->send_P(200, "text/plain", String(runtime).c_str());  
+      request->send_P(200, "text/plain", String(runtime).c_str());
     }
-    
+
   });
 
   server.on("/frequency", HTTP_GET, [](AsyncWebServerRequest * request) {
@@ -353,7 +370,7 @@ bool setUpServer(){
   server.on("/isSystemWatering", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/plain", String(isSystemWatering).c_str());
   });
-  
+
   server.on("/getFrequency", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/plain", String(frequency_hours).c_str());
   });
@@ -366,18 +383,18 @@ bool setUpServer(){
       request->send_P(200, "text/plain", "off");
     }
     else{
-      request->send_P(200, "text/plain", String(runtime).c_str());  
+      request->send_P(200, "text/plain", String(runtime).c_str());
     }
-    
+
   });
 
   server.on("/getTimeLeft", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/plain", String(time_left_until_next_watering_min).c_str());
   });
-  
+
   // Start server
   server.begin();
-  
+
   // Initialize SPIFFS
   if (!SPIFFS.begin(true)) {
     Serial.println("An Error has occurred while mounting SPIFFS");
@@ -391,14 +408,22 @@ bool setUpServer(){
 
 void setup() {
   Serial.begin(115200);
-  
+  xMutex = xSemaphoreCreateMutex();
   init();
- 
+
   if (!setUpServer()){
     Serial.println("An Error has occurred while setUpServer(). STOPPING.");
     return;
   }
-  
+
+   xTaskCreate(
+    frequencyTask,                    // Function that should be called
+    "Initiating frequencyTask",       // Name of the task (for debugging)
+    1000,                             // Stack size (bytes)
+    NULL,            // Parameter to pass
+    1,               // Task priority
+    NULL             // Task handle
+  );
   // ****************************** OTA ************************
   ArduinoOTA
   .onStart([]() {
